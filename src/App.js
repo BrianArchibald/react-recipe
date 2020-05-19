094218ec1e1c7b6fe9ae45cd3a88f734c9c30539
Brian Archibald <brianallenarchibald@gmail.com>
2020-05-18 19:40:50 -0500

    click counter example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -9,14 +9,19 @@ const APP_KEY = process.env.REACT_APP_KEY;
 const exampleRequest = 
     `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+const [counter, setCounter] = useState(0);
 
+useEffect(() => {
+  console.log('Effect ran');
+});
 
   return(
     <div className="App">
-      <form>
-        <input type="text"/>
-        <button type="submit">Search</button>
-      </form>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+        </form>
+      <h2 onClick={() => setCounter(counter + 1)} className="search-button" type="submit">{counter}</h2>
     </div>
   );
 }
