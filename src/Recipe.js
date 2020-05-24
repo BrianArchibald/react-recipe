e4df5a1a3fd828db32e2dfa99575099fa373f43a
Brian Archibald <brianallenarchibald@gmail.com>
2020-05-23 20:21:37 -0500

    updated css and recipe props

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Recipe = ({ title, calories, image }) => { // the props we are passing down from App.js
+const Recipe = ({ title, calories, image, ingredients }) => { // the props we are passing down from App.js
     return(
         <div>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>
+                        {ingredients.text}
+                    </li>
+                ))}
+            </ol>
             <p>{calories}</p>
             <img src={image} alt="" />
         </div>
